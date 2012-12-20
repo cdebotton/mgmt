6b57dca04cb739b47c9ee9266bdd17732907057b
Christian de Botton <cdebotton@gmail.com>
2012-12-19 19:05:33 -0500

    Added media temple locality.
    Signed-off-by: Christian de Botton <cdebotton@gmail.com>

--- paths.php
+++ paths.php
@@ -23,7 +23,8 @@
 
 $environments = array(
 
-	'local' => array('http://localhost*', '*.dev'),
+	'local'			=> array('http://localhost*', '*.dev'),
+	'mediatemple'	=> array('http://mgmt.christiandebotton.com'),
 
 );
 
