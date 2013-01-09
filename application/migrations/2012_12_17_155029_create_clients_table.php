4a9b3c43c437e8503ac1558a9833777b8efa2a4e
Christian de Botton <cdebotton@gmail.com>
2013-01-08 19:28:08 -0500

    Added client seeds to the client migration.

--- application/migrations/2012_12_17_155029_create_clients_table.php
+++ application/migrations/2012_12_17_155029_create_clients_table.php
@@ -16,6 +16,131 @@ class Create_Clients_Table {
 			$table->text('memo');
 			$table->timestamps();
 		});
+
+		Client::create(array(
+			'id' 		=> 1,
+			'name'		=> 'Busby'
+		));
+
+		Client::create(array(
+			'id' 		=> 2,
+			'name'		=> 'Toshiko Mori'
+		));
+
+		Client::create(array(
+			'id' 		=> 3,
+			'name'		=> 'Brooklyn Arts Council'
+		));
+
+		Client::create(array(
+			'id' 		=> 4,
+			'name'		=> 'Peter Rose & Partners'
+		));
+
+		Client::create(array(
+			'id' 		=> 5,
+			'name'		=> 'Vertebra'
+		));
+
+		Client::create(array(
+			'id' 		=> 6,
+			'name'		=> 'Todd Williams & Billie Tsien Architects'
+		));
+
+		Client::create(array(
+			'id' 		=> 7,
+			'name'		=> 'RS Granoff Architects'
+		));
+
+		Client::create(array(
+			'id' 		=> 8,
+			'name'		=> 'AT&T'
+		));
+
+		Client::create(array(
+			'id' 		=> 9,
+			'name'		=> 'Gucci'
+		));
+
+		Client::create(array(
+			'id' 		=> 10,
+			'name'		=> 'Levi\'s'
+		));
+
+		Client::create(array(
+			'id' 		=> 11,
+			'name'		=> 'Guggenheim'
+		));
+
+		Client::create(array(
+			'id' 		=> 12,
+			'name'		=> 'Gary Hutswit Films'
+		));
+
+		Client::create(array(
+			'id' 		=> 13,
+			'name'		=> 'Associated Press'
+		));
+
+		Client::create(array(
+			'id' 		=> 14,
+			'name'		=> 'Mary Bright'
+		));
+
+		Client::create(array(
+			'id' 		=> 15,
+			'name'		=> '80 Metropolitan'
+		));
+
+		Client::create(array(
+			'id' 		=> 16,
+			'name'		=> 'Governer\'s Island'
+		));
+
+		Client::create(array(
+			'id' 		=> 17,
+			'name'		=> 'Raison Pure'
+		));
+
+		Client::create(array(
+			'id' 		=> 18,
+			'name'		=> 'Peter Walker Partners'
+		));
+
+		Client::create(array(
+			'id' 		=> 19,
+			'name'		=> 'Brilliant Transportation'
+		));
+
+		Client::create(array(
+			'id' 		=> 20,
+			'name'		=> 'Alloy Development'
+		));
+
+		Client::create(array(
+			'id' 		=> 21,
+			'name'		=> 'Sagmeister Inc.'
+		));
+
+		Client::create(array(
+			'id' 		=> 22,
+			'name'		=> 'Columbia University'
+		));
+
+		Client::create(array(
+			'id' 		=> 23,
+			'name'		=> 'Steiner Studios'
+		));
+
+		Client::create(array(
+			'id' 		=> 24,
+			'name'		=> 'Corcoran Group'
+		));
+
+		Client::create(array(
+			'id' 		=> 25,
+			'name'		=> 'Droog'
+		));
 	}
 
 	/**
